d0b3d645ae96de8c88c2d6022bd6f38a592bf25e
98YooJunHo <dbwnsgh2000@naver.com>
2023-06-05 00:11:02 +0900

    Feat : 걷는 모험가 이야기
    악마 출현 이벤트 추가
    그 외에 5월자 파일 위치이동

---
+++ cpp/23_06/230602/WalkingAdventuerTest.cpp
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//#include <conio.h>
+//#include <iostream>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <windows.h>
+//
+//int loop = 0;
+//int playerMhp = 50;
+//int playeratk = 5;
+//int playerhp = playerMhp;
+//int pray = 0;
+//
+//int main()
+//{
+//	printf("\n****************************************\n\n");
+//	printf("\n\n\n       숲의 요정의 기도를 받습니다\n");
+//	printf("       \"부디 악마를 물리쳐 주세요\"\n\n\n");
+//	printf("\n****************************************\n\n");
+//	Sleep(1500);
+//	pray = 1;
+//	system("cls");
+//	return 0;
+//}
--- cpp/23_06/230602/WalkingAdventure_YJH.cpp
+++ cpp/23_06/230602/WalkingAdventure_YJH.cpp
@@ -5,34 +5,27 @@
 #include <time.h>
 #include <windows.h>
 
-int Battle(int playerhp, int playerMhp, int playeratk);
-int ForestEvent(int playerMhp), Forest();
-int RiverEvent(int playeratk), River();
-int Mountain();
+int Battle(), Hell(), HellEvent();
+int ForestEvent(), Forest();
+int RiverEvent(), River();
+int Mountain(), Start();
 
 int loop = 0;
+int playerMhp = 50;
+int playeratk = 5;
+int playerhp = playerMhp;
+int pray = 0;
 
 int main()
 {
-	int playerMhp = 50;
-	int playeratk = 5;
-	int playerhp = playerMhp;
 	char power = '0';
 	srand(time(NULL));
 
-	printf("\n****************************************\n\n");
-	printf("****************************************\n");
-	printf("****************************************\n");			// 게임
-	printf("****** 걸어 다니는 모험가 이야기 *******\n");				// 시작
-	printf("****************************************\n");			// 로고
-	printf("****************************************\n");			
-	printf("****************************************\n\n");
-	printf("     시작 하려면 아무 키나 누르세요\n");
-	printf("\n****************************************\n\n");
+	Start();
 	power = _getch();
 	system("cls");
 
-	while (6)										// 반복문 while
+	while (loop < 6)										// 반복문 while
 	{
 		power = '0';
 		loop += 1;
@@ -70,22 +63,30 @@ int main()
 		if (power == 'w' || power == 'W')					// 걷기 조건문 걷기 if
 		{
 			int wrandom = rand() % 100 + 1;					// 1 ~ 100 사이의 랜덤 수를 길 랜덤에 저장
+
+			if (wrandom <= 1 || loop == 100)				// 1 퍼센트 확률로 지옥에 진입
+			{
+				Hell();
+				HellEvent();
+				continue;
+			}
+
 			if (wrandom <= 30)								// 길 랜덤이 30이하일 때 실행하는 강 if
 			{
 				Forest();
-				playerMhp = ForestEvent(playerMhp);
+				ForestEvent();
 				continue;
 			}												// 숲 if
 			else if (wrandom <= 50)							// 길 랜덤이 31이상 50이하일 때 실행하는 강 if
 			{
 				River();
-				playeratk = RiverEvent(playeratk);
+				RiverEvent();
 				continue;
 			}												// 강 if()
 			else											// 길 랜덤이 51이상 100이하일 때 실행하는 산 else
 			{
 				Mountain();
-				playerhp = Battle(playerhp, playerMhp, playeratk); // Battle 함수 실행 후 체력값을 받아서 현재 체력에 입력
+				Battle(); // Battle 함수 실행 후 체력값을 받아서 현재 체력에 입력
 				continue;
 			}												// 산 else()
 		}													// 걷기 if()
@@ -101,10 +102,24 @@ int main()
 	return 0;
 }															// main()
 
+int Start()
+{
+	printf("\n****************************************\n\n");
+	printf("****************************************\n");
+	printf("****************************************\n");			// 게임
+	printf("****** 걸어 다니는 모험가 이야기 *******\n");				// 시작
+	printf("****************************************\n");			// 로고
+	printf("****************************************\n");
+	printf("****************************************\n\n");
+	printf("     시작 하려면 아무 키나 누르세요\n");
+	printf("\n****************************************\n\n");
+	return 0;
+}
+
 int Mountain()												// 산 입장함수
 {
 	printf("\n****************************************\n\n");
-	printf("\n             걸 ");
+	printf("\n            걸 ");
 	Sleep(500);
 	printf(" 어 ");
 	Sleep(500);
@@ -115,28 +130,29 @@ int Mountain()												// 산 입장함수
 	printf("       *       산입니다        *\n");
 	printf("       *************************\n\n");
 	printf("\n****************************************\n\n");
-	Sleep(1000);
+	Sleep(1500);
 	system("cls");
 	return 0;
 }
 
-int Battle(int playerhp, int playerMhp, int playeratk)
+int Battle()
 {
 	int battle = rand() % 100 + 1;
 	bool battle40 = battle < 41;
 
-	switch (battle40)						// 1 ~ 100 사이의 수를 생성했을 때 40 이하라면 switch (1)
+	switch (battle40)							// 1 ~ 100 사이의 수를 생성했을 때 40 이하라면 switch (1)
 	{
 	case 1:													// switch(1) 일때 스코프 내부 실행
 	{
-		int monsterMhp = (25 + loop / 2), monsteratk = 4;	// 몬스터의 최대체력, 공격력 선언 및 초기화
-		int matkrandom = rand() % 100 + 1;					// 1 ~ 100 수를 받아 몬스터공격력증가 확률에 대입
-		char battle = '0';
-		if (matkrandom < 31)								// matkrandom이 30이하라면 (30퍼센트)
-		{
-			monsteratk += 1;								// 몬스터공격력 + 1
-		}
+		int monsterMhp = 20, monsteratk = 4;				// 몬스터의 최대체력, 공격력 선언 및 초기화
+		monsterMhp += (loop / 2 + 5);						// (5 + 루프횟수/2)만큼 몬스터 최대체력 증가
 		int monsterhp = monsterMhp;
+		monsteratk += (loop / 3);
+		int matkcrit = monsteratk * 2;
+		int patkcrit = playeratk * 2;
+
+		char battle = '0';
+
 		printf("\n****************************************\n\n");
 		printf("\n\n         몬스터를 만났습니다!!\n");
 		printf("      화가 난 몬스터와 전투합니다\n");
@@ -146,46 +162,85 @@ int Battle(int playerhp, int playerMhp, int playeratk)
 		system("cls");
 		while (1)
 		{
-			printf("\n****************************************\n\n");
-			printf("\n\n\n       몬스터에게 공격받았습니다\n");
-			printf("         %d의 피해를 입었습니다\n",monsteratk);
-			playerhp -= monsteratk;							// 플레이어의 현재체력
-			(playerhp < 0) ? playerhp = 0 : playerhp = playerhp;
-			printf("         현재 내 체력 : %d/%d\n\n", playerhp, playerMhp);	// 플레이어의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
-			printf("\n****************************************\n\n");
-			Sleep(1000);
-			system("cls");
+			int mcrit = rand() % 100 + 1;					// 몬스터 크리 변수 1 ~ 100
+			int pcrit = rand() % 100 + 1;					// 유저 크리 변수 1 ~ 100
+
+			if (mcrit <= 5)									// 5퍼센트확률로 몬스터 크리티컬 공격
+			{
+				printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+				printf("             몬스터의 턴\n\n");
+				printf("\n            !!크리티컬!!\n");
+				printf("         %d의 피해를 받았습니다\n", matkcrit);
+				playerhp -= matkcrit;							// 플레이어의 현재체력
+				(playerhp < 0) ? playerhp = 0 : playerhp = playerhp;
+				printf("         현재 내 체력 : %d/%d\n\n", playerhp, playerMhp);	// 플레이어의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+				printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+				Sleep(1500);
+				system("cls");
+			}
+			else 
+			{
+				printf("\n****************************************\n\n");
+				printf("             몬스터의 턴\n\n");
+				printf("\n       몬스터에게 공격받았습니다\n");
+				printf("         %d의 피해를 입었습니다\n", monsteratk);
+				playerhp -= monsteratk;							// 플레이어의 현재체력
+				(playerhp < 0) ? playerhp = 0 : playerhp = playerhp;
+				printf("         현재 내 체력 : %d/%d\n\n", playerhp, playerMhp);	// 플레이어의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+				printf("\n****************************************\n\n");
+				Sleep(1300);
+				system("cls");
+			}
+
 			if (playerhp <= 0)								// 플레이어의 현재체력이 0이하일 경우 while 문 종료
 			{
 				printf("\n****************************************\n\n");
 				printf("\n\n\n           화가 난 몬스터에게\n");
 				printf("        그만 죽고 말았습니다...\n\n\n");
 				printf("\n****************************************\n\n");
-				Sleep(1000);
+				Sleep(2000);
 				system("cls");
 				break;						
 			}
 
-			printf("\n****************************************\n\n");
-			printf("\n\n\n          몬스터를 공격합니다\n");
-			printf("         %d의 피해를 입혔습니다\n",playeratk);
-			monsterhp -= playeratk;						// 몬스터의 현재체력
-			(monsterhp < 0) ? monsterhp = 0 : monsterhp = monsterhp;
-			printf("       현재 몬스터 체력 : %d/%d\n\n", monsterhp, monsterMhp);		// 몬스터의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
-			printf("\n****************************************\n\n");
-			Sleep(1000);
-			system("cls");
+			if (pcrit <= 15)								// 15퍼센트 확률로 유저 크리티컬 공격
+			{
+				printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+				printf("             모험가의 턴\n\n");
+				printf("\n             !!크리티컬!!\n");
+				printf("         %d의 피해를 입혔습니다\n", patkcrit);
+				monsterhp -= patkcrit;						// 몬스터의 현재체력
+				(monsterhp < 0) ? monsterhp = 0 : monsterhp = monsterhp;
+				printf("        현재 몬스터 체력 : %d/%d\n\n", monsterhp, monsterMhp);		// 몬스터의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+				printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+				Sleep(1500);
+				system("cls");
+			}
+			else
+			{
+				printf("\n****************************************\n\n");
+				printf("             모험가의 턴\n\n");
+				printf("\n          몬스터를 공격합니다\n");
+				printf("         %d의 피해를 입혔습니다\n", playeratk);
+				monsterhp -= playeratk;						// 몬스터의 현재체력
+				(monsterhp < 0) ? monsterhp = 0 : monsterhp = monsterhp;
+				printf("        현재 몬스터 체력 : %d/%d\n\n", monsterhp, monsterMhp);		// 몬스터의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+				printf("\n****************************************\n\n");
+				Sleep(1300);
+				system("cls");
+			}
+
 			if (monsterhp <= 0)							// 몬스터의 현재체력이 0이하일 경우 while 문 종료
 			{
 				printf("\n****************************************\n\n");
 				printf("\n\n\n      몬스터를 무찌르고 돌아갑니다\n\n\n\n");
 				printf("\n****************************************\n\n");
-				Sleep(1000);
+				Sleep(1500);
 				system("cls");
 				break;
 			}
 		}												// while(1)
-		return playerhp;								// case 1이 끝날 때 playerhp 반환
+		return 0;										// case 1이 끝날 때 Battle()종료
 	}													// case 1:
 	default:
 	{
@@ -194,11 +249,11 @@ int Battle(int playerhp, int playerMhp, int playeratk)
 		printf("\n\n\n        산 속의 옹달샘을 마시고\n");
 		printf("        %d만큼 체력을 회복합니다\n\n\n", randomheal);
 		printf("\n****************************************\n\n");
-		Sleep(1000);
+		Sleep(1500);
 		system("cls");
 		playerhp += randomheal;							// 옹달샘을 마실경우 1 ~ 최대체력수치 랜덤 회복
 		(playerhp > playerMhp) ? playerhp = playerMhp : playerhp = playerhp;	// 현재체력이 최대체력을 넘을경우 최대체력수치로 고정
-		return playerhp;								// default가 끝날 때 playerhp 반환
+		return 0		;								// default가 끝날 때 Battle()종료
 	}													// default:
 	}													// switch()
 }
@@ -217,35 +272,55 @@ int Forest()
 	printf("       *       숲입니다        *\n");
 	printf("       *************************\n\n");
 	printf("\n****************************************\n\n");
-	Sleep(1000);
+	Sleep(1500);
 	system("cls");
 	return 0;
 }
 
-int ForestEvent(int playerMhp)
+int ForestEvent()
 {
 	int frandom = rand() % 100 + 1;				// 1~100사이의 랜덤수를 강 랜덤에 저장
+	if (pray == 0)
+	{
+		if (frandom < 56 || frandom > 50)
+		{
+			printf("\n****************************************\n\n");
+			printf("\n\n\n       숲의 요정의 기도를 받습니다\n");
+			printf("       \"부디 악마를 물리쳐 주세요\"\n\n\n");
+			printf("\n****************************************\n\n");
+			Sleep(1500);
+			system("cls");
+			pray = 1;
+			return 0;
+		}
+	}
+
 	if (frandom < 51)							// 숲 랜덤이 50이하 라면 최대체력 + 1
 	{
 		printf("\n****************************************\n\n");
 		printf("\n\n\n    숲 근처의 약초를 찾아 먹습니다\n");
-		printf("         최대체력이 증가합니다\n\n\n");
+		printf("        최대체력이 3증가합니다\n\n\n");
 		printf("\n****************************************\n\n");
-		Sleep(1000);
+		Sleep(1500);
 		system("cls");
-		return (playerMhp += 1);
+		playerMhp += 3;
+		return 0;
 	}
 	printf("\n****************************************\n\n");
-	printf("\n\n\n   잘려진 나무밑동에 앉아 휴식합니다\n\n\n\n");	// 숲 랜덤이 50이하가 아니라면 휴식
+	printf("\n\n\n   잘려진 나무밑동에 앉아 휴식합니다\n");	// 숲 랜덤이 50이하가 아니라면 휴식
+	printf("        10만큼 체력을 회복합니다\n\n\n");
 	printf("\n****************************************\n\n");
-	Sleep(1000);
+	Sleep(1500);
 	system("cls");
-	return playerMhp;
+	playerhp += 10;
+	(playerhp > playerMhp) ? playerhp = playerMhp : playerhp = playerhp;
+	return 0;
 }
+
 int River()
 {
 	printf("\n****************************************\n\n");
-	printf("\n             걸 ");
+	printf("\n            걸 ");
 	Sleep(500);
 	printf(" 어 ");
 	Sleep(500);
@@ -256,28 +331,157 @@ int River()
 	printf("       *       강입니다        *\n");
 	printf("       *************************\n\n");
 	printf("\n****************************************\n\n");
-	Sleep(1000);
+	Sleep(1500);
 	system("cls");
 	return 0;
 }
 
-int RiverEvent(int playeratk)
+int RiverEvent()
 {
 	int rrandom = rand() % 100 + 1;				// 1~100사이의 랜덤수를 강 랜덤에 저장
 	if (rrandom < 51)							// 강 랜덤이 50이하 라면 공격력 + 1
 	{
 		printf("\n****************************************\n\n");
 		printf("\n\n\n  강 근처의 부싯돌을 찾아 칼을 갑니다\n");
-		printf("          공격력이 증가합니다\n\n\n");
+		printf("         공격력이 2증가합니다\n\n\n");
 		printf("\n****************************************\n\n");
-		Sleep(1000);
+		Sleep(1500);
 		system("cls");
-		return (playeratk += 1);
+		playeratk += 2;
+		return 0;
 	}
 	printf("\n****************************************\n\n");
 	printf("\n\n\n    강가에 앉아 경치를 둘러 봅니다\n\n\n\n");	// 강 랜덤이 50이하가 아니라면 경치구경
 	printf("\n****************************************\n\n");
-	Sleep(1000);
+	Sleep(1500);
 	system("cls");
-	return playeratk;
+	return 0;
+}
+
+int Hell()
+{
+	printf("\n****************************************\n\n");
+	printf("\n            걸 ");
+	Sleep(500);
+	printf(" 어 ");
+	Sleep(500);
+	printf(" 간 ");
+	Sleep(500);
+	printf(" 다 \n");
+	printf("\n             썩 좋지 못한\n");
+	printf("            기분이 듭니다...\n\n\n");
+	printf("\n****************************************\n\n");
+	Sleep(2500);
+	system("cls");
+
+	printf("\n");
+	for (int i = 0; i < 40; i += 1)
+	{
+		printf("!");
+		Sleep(50);
+	}
+	printf("\n\n");
+	printf("\n\n\n         지옥의 악마를 마주합니다\n\n\n\n");
+	printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+	Sleep(2000);
+	system("cls");
+	return 0;
+}
+
+int HellEvent()
+{
+	int devilMhp = 100, devilatk = 10;
+	devilMhp += loop;
+	devilatk += (loop/2);
+	int devilhp = devilMhp;
+	int datkcrit = devilatk * 100;
+	int patkcrit = playeratk * 2;
+	int pcritper = 15;
+	if (pray == 1)
+	{
+		patkcrit = playeratk * 3;
+		pcritper = 30;
+	}
+
+	while (1)
+	{
+		int dcrit = rand() % 100 + 1;					// 악마 크리 변수 1 ~ 100
+		int pcrit = rand() % 100 + 1;					// 유저 크리 변수 1 ~ 100
+
+		if (dcrit <= 1)									// 1퍼센트확률로 악마 크리티컬 공격
+		{
+			printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+			printf("              악마의 턴\n\n");
+			printf("\n            !!크리티컬!!\n");
+			printf("       %d의 피해를 받았습니다\n", datkcrit);
+			playerhp -= datkcrit;							// 플레이어의 현재체력
+			(playerhp < 0) ? playerhp = 0 : playerhp = playerhp;
+			printf("          현재 내 체력 : %d/%d\n\n", playerhp, playerMhp);	// 플레이어의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+			printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+			Sleep(2000);
+			system("cls");
+		}
+		else
+		{
+			printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+			printf("              악마의 턴\n\n");
+			printf("\n        악마에게 공격받았습니다\n");
+			printf("         %d의 피해를 입었습니다\n", devilatk);
+			playerhp -= devilatk;							// 플레이어의 현재체력
+			(playerhp < 0) ? playerhp = 0 : playerhp = playerhp;
+			printf("         현재 내 체력 : %d/%d\n\n", playerhp, playerMhp);	// 플레이어의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+			printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+			Sleep(1300);
+			system("cls");
+		}
+
+		if (playerhp <= 0)								// 플레이어의 현재체력이 0이하일 경우 while 문 종료
+		{
+			printf("\n****************************************\n\n");
+			printf("\n\n\n            지옥의 악마에게\n");
+			printf("        그만 죽고 말았습니다...\n\n\n");
+			printf("\n****************************************\n\n");
+			Sleep(2000);
+			system("cls");
+			break;
+		}
+
+		if (pcrit <= pcritper)								// 15퍼센트 확률로 유저 크리티컬 공격
+		{
+			printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+			printf("             모험가의 턴\n\n");
+			printf("\n             !!크리티컬!!\n");
+			printf("    크리티컬!! %d의 피해를 입혔습니다\n", patkcrit);
+			devilhp -= patkcrit;						// 악마의 현재체력
+			(devilhp < 0) ? devilhp = 0 : devilhp = devilhp;
+			printf("        현재 악마 체력 : %d/%d\n\n", devilhp, devilMhp);		// 악마의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+			printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+			Sleep(1500);
+			system("cls");
+		}
+		else
+		{
+			printf("\n****************************************\n\n");
+			printf("             모험가의 턴\n\n");
+			printf("\n           악마를 공격합니다\n");
+			printf("         %d의 피해를 입혔습니다\n", playeratk);
+			devilhp -= playeratk;						// 악마의 현재체력
+			(devilhp < 0) ? devilhp = 0 : devilhp = devilhp;
+			printf("        현재 악마 체력 : %d/%d\n\n", devilhp, devilMhp);		// 몬스터의 현재체력이 0미만일 경우 0으로 0미만이 아닐경우 유지
+			printf("\n****************************************\n\n");
+			Sleep(1300);
+			system("cls");
+		}
+
+		if (devilhp <= 0)							// 악마의 현재체력이 0이하일 경우 while 문 종료
+		{
+			printf("\n****************************************\n\n");
+			printf("\n\n\n       악마를 무찌르고 돌아갑니다\n\n\n\n");
+			printf("\n****************************************\n\n");
+			Sleep(1500);
+			system("cls");
+			break;
+		}
+	}
+	return 0;
 }
